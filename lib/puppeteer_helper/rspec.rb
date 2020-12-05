3fe9303795b692b9b3272cc8a7a593ec17c5c2d7
aki <lala.akira@gmail.com>
2020-12-06 08:48:43 +0900

    refactor: Rewritten comments

--- lib/puppeteer_helper/rspec.rb
+++ lib/puppeteer_helper/rspec.rb
@@ -1,10 +1,12 @@
 RSpec.configure do
-  # NOTE: puppeteer経由でchromiumのバージョンを管理している
+  # NOTE: Using Chromium included in Puppeteer
   module_path = Rails.root.join('node_modules/puppeteer')
   chromium_revision = `node -e "process.stdout.write(require('#{module_path}/lib/cjs/puppeteer/revisions').PUPPETEER_REVISIONS.chromium)"`
   chromium_path = `node -e "process.stdout.write(require('puppeteer').createBrowserFetcher().revisionInfo('#{chromium_revision}').executablePath)"`
   Selenium::WebDriver::Chrome.path = chromium_path
-  # NOTE: Chromiumの場合、環境やバージョンによってwebdrivers gemの自動バージョン判定が正しく機能しないので自前で判定している
+  # NOTE: Detect from the major version
+  # SEE: https://chromedriver.chromium.org/downloads/version-selection
+  # SEE: https://github.com/titusfortner/webdrivers/issues/180
   chromium_major_version = Webdrivers::ChromeFinder.version.split('.').first
   Webdrivers::Chromedriver.required_version = Webdrivers::Network.get("https://chromedriver.storage.googleapis.com/LATEST_RELEASE_#{chromium_major_version}")
 end
